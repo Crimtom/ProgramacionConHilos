50a6eadea9ec8e910384b8cfffc490ee893fbc9b
Crimcat <69399291+Crimtom@users.noreply.github.com>
2025-11-03 19:02:11 -0600

    Agregar archivos de proyecto.

---
+++ ProgramacionConHilos/PROGRAM_MAIN.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramacionConHilos
+{
+    public partial class PROGRAM_MAIN : Form
+    {
+        public PROGRAM_MAIN()
+        {
+            InitializeComponent();
+        }
+    }
+}
