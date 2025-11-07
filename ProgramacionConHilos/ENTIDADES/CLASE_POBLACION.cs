b879a8c63e100336136f0b8f645d357f4728a1e1
Crimcat <69399291+Crimtom@users.noreply.github.com>
2025-11-06 20:52:01 -0600

    Clases creadas y CLASE_POBLACION completada (Falta testing)

---
+++ ProgramacionConHilos/ENTIDADES/CLASE_POBLACION.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramacionConHilos.ENTIDADES
+{
+    internal class CLASE_POBLACION
+    {
+
+        #region ATRIBUTOS
+        public int Edad { get; set; }
+        public int Hombres { get; set; }
+        public int Mujeres { get; set; }
+
+        public int[] Nivel_Educacion = { 0, 0, 0, 0, 0, 0, 0 };
+        #endregion
+
+        public void Poblacion(int PEdad, int PHombres, int PMujeres, int[] PNivel_Educacion)
+        {
+            Edad = PEdad;
+            Hombres = PHombres;
+            Mujeres = PMujeres;
+            Nivel_Educacion = PNivel_Educacion;
+        }
+    }
+}
---
+++ ProgramacionConHilos/LOGICA/CLASE_UTILIDAD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramacionConHilos.LOGICA
+{
+    internal class CLASE_UTILIDAD
+    {
+        public void Salir()
+        {
+            if (MessageBox.Show("¿Desea salir de la aplicación?", "Salir", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
