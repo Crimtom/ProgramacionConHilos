38064fa6fa96a7ec6ddf556d72184ecddcf6862f
Crimcat <69399291+Crimtom@users.noreply.github.com>
2025-11-07 20:23:00 -0600

    Declaración de atributos en el Formulario de Windows

--- ProgramacionConHilos/PRESENTACION/PROGRAM_MAIN.cs
+++ ProgramacionConHilos/PRESENTACION/PROGRAM_MAIN.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using ProgramacionConHilos.ENTIDADES;
+using ProgramacionConHilos.LOGICA;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,6 +15,20 @@ namespace ProgramacionConHilos
 {
     public partial class PROGRAM_MAIN : Form
     {
+
+        static CLASE_POBLACION[] Array_Personas = new CLASE_POBLACION[40];
+        static int Indice_Persona = 0;
+        static int Can_Hombres = 0;
+        static int Can_Mujeres = 0;
+
+        CLASE_UTILIDAD Obj_Util = new CLASE_UTILIDAD();
+
+        Thread Obj_T1; // AGRUPAR LAS MUJERES
+        Thread Obj_T2; // AGRUPAR LOS HOMBRES
+        Thread Obj_T3; // AGRUPAR LAS EDADES SEGÚN EL SEXO DE LAS PERSONAS
+        Thread Obj_T4; // AGRUPAR LOS SEXOS SEGÚN LA EDAD DE LAS PERSONAS
+        Thread Obj_T5; // AGRUPAR SEGÚN EL NIVEL DE EDUCACIÓN
+
         public PROGRAM_MAIN()
         {
             InitializeComponent();
