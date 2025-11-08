0fd3f080679820bf34ff6dbbf19b7476b710c638
Crimcat <69399291+Crimtom@users.noreply.github.com>
2025-11-07 20:35:08 -0600

    Se agregó funcionalidad al botón destinado a salir del programa.

--- ProgramacionConHilos/PRESENTACION/PROGRAM_MAIN.cs
+++ ProgramacionConHilos/PRESENTACION/PROGRAM_MAIN.cs
@@ -41,7 +41,44 @@ namespace ProgramacionConHilos
 
         private void BT_SALIR_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                if (Obj_Util.Salir() == 1)
+                {
+                    if (Obj_T1 != null)
+                    {
+                        if (Obj_T1.ThreadState == ThreadState.WaitSleepJoin)
+                            Obj_Util.Mensaje("El Hilo de Agrupar Mujeres esta en Ejecución, No se puede Salir del Programa");
+
+                        if (Obj_T2 != null)
+                            if ((Obj_T2.ThreadState == ThreadState.WaitSleepJoin) && (Obj_T1 != null))
+                                Obj_Util.Mensaje("El Hilo de Agrupar Hombres esta en Ejecución, No se puede Salir del Programa");
+
+                        if (Obj_T3 != null)
+                            if ((Obj_T3.ThreadState == ThreadState.WaitSleepJoin) && (Obj_T1 != null))
+                                Obj_Util.Mensaje("El Hilo de Agrupar las edades según el sexo de las personas esta en Ejecución, No se puede Salir del Programa");
+
+                        if (Obj_T4 != null)
+                            if ((Obj_T4.ThreadState == ThreadState.WaitSleepJoin) && (Obj_T1 != null))
+                                Obj_Util.Mensaje("El Hilo de Agrupar los sexos según las edades de las personas esta en Ejecución, No se puede Salir del Programa");
+
+
+                        if (Obj_T5 != null)
+                            if ((Obj_T5.ThreadState == ThreadState.WaitSleepJoin) && (Obj_T1 != null))
+                                Obj_Util.Mensaje("El Hilo de Agrupar a las personas según su nivel su escolaridad esta en Ejecución, No se puede Salir del Programa");
+                            else
+                                this.Dispose();
+                    }
+                    else
+                    {
+                        this.Dispose();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Se ha producido un error tras intentar cerrar el programa: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }  
         }
     }
 }
